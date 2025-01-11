8933ec486b759d4bca0aee95eb80ab6d06ddd100
Kadir Lofca <kadirlofca@outlook.com>
2025-01-10 22:30:27 -0500

    added logo component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,13 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+		extend: {
+			colors: {
+				surface: {
+					'dark': "hsl(var(--surface-dark) / <alpha-value>)",
+					'base': "hsl(var(--surface-base) / <alpha-value>)",
+					'light': "hsl(var(--surface-light) / <alpha-value>)",
+				},
       },
     },
   },
