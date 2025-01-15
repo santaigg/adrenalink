9b5346cd4676c03b22ef03ec6d89aab56972e111
Kadir Lofca <kadirlofca@outlook.com>
2025-01-15 18:22:58 -0500

    changed names of css variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,51 +10,6 @@ export default {
   theme: {
     extend: {
       colors: {
-        surface: {
-          0: "hsl(var(--surface-0) / <alpha-value>)",
-          1: "hsl(var(--surface-1) / <alpha-value>)",
-          2: "hsl(var(--surface-2) / <alpha-value>)",
-        },
-        
-        primary: {
-          DEFAULT: "hsl(var(--primary) / <alpha-value>)",
-          foreground: "hsl(var(--primary-foreground)  / <alpha-value>)",
-        },
-
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
-        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
