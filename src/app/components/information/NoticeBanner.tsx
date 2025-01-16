6d0bbee24e7287e546cd8a557f7d437df5450ec6
Kadir Lofca <kadirlofca@outlook.com>
2025-01-16 18:38:18 -0500

    Added extrusion to notice banner

--- src/app/components/information/NoticeBanner.tsx
+++ src/app/components/information/NoticeBanner.tsx
@@ -1,13 +1,17 @@
 import { cn } from "../../utils/cn";
+import Extrusion, { CornerLocation } from "../cosmetic/Extrusion";
 
 export default function NoticeBanner({ className, notice }: { className?: string; notice: string }) {
   return (
-    <div className="flex flex-row">
-      <div className="min-w-4 bg-accent"></div>
-      <p className="w-max mx-4 text-warning text-lg font-black">{notice}</p>
-      <div className="mr-4 min-w-4 bg-accent"></div>
-      <div className={cn("w-full p-2 bg-[repeating-linear-gradient(45deg,#c6fb04_0px,#c6fb04_20px,transparent_20px,transparent_40px)]", className)}></div>
-      <div className="ml-4 min-w-4 bg-accent"></div>
+    <div>
+      <div className="flex flex-row">
+        <div className="min-w-3 bg-accent"></div>
+        <p className="w-max mx-4 text-warning text-lg font-black">{notice}</p>
+        <div className="mr-4 min-w-3 bg-accent"></div>
+        <div className={cn("w-full p-2 bg-[repeating-linear-gradient(45deg,#c6fb04_0px,#c6fb04_20px,transparent_20px,transparent_40px)]", className)}></div>
+        <div className="ml-4 min-w-3 bg-accent"></div>
+      </div>
+      <Extrusion className="mt-4 float-right min-w-[20%] border-accent" cornerLocation={CornerLocation.BottomLeft} />
     </div>
   );
 }
