8893fd962dc670388e7c29dca577fd61d8d2c8c4
Kadir Lofca <kadirlofca@outlook.com>
2025-01-16 18:40:01 -0500

    Increased noticebanner extrusion size

--- src/app/components/information/NoticeBanner.tsx
+++ src/app/components/information/NoticeBanner.tsx
@@ -11,7 +11,7 @@ export default function NoticeBanner({ className, notice }: { className?: string
         <div className={cn("w-full p-2 bg-[repeating-linear-gradient(45deg,#c6fb04_0px,#c6fb04_20px,transparent_20px,transparent_40px)]", className)}></div>
         <div className="ml-4 min-w-3 bg-accent"></div>
       </div>
-      <Extrusion className="mt-4 float-right min-w-[20%] border-accent" cornerLocation={CornerLocation.BottomLeft} />
+      <Extrusion className="mt-4 float-right min-w-[40%] border-accent" cornerLocation={CornerLocation.BottomLeft} />
     </div>
   );
 }
