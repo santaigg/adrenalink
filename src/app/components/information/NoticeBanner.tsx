8a98de6a2aea37f735d70ca562b0b331c0ca980d
Kadir Lofca <kadirlofca@outlook.com>
2025-01-16 18:43:19 -0500

    Decreased distance between each strip on noticebanner

--- src/app/components/information/NoticeBanner.tsx
+++ src/app/components/information/NoticeBanner.tsx
@@ -8,7 +8,7 @@ export default function NoticeBanner({ className, notice }: { className?: string
         <div className="min-w-3 bg-accent"></div>
         <p className="w-max mx-4 text-warning text-lg font-black">{notice}</p>
         <div className="mr-4 min-w-3 bg-accent"></div>
-        <div className={cn("w-full p-2 bg-[repeating-linear-gradient(45deg,#c6fb04_0px,#c6fb04_20px,transparent_20px,transparent_40px)]", className)}></div>
+        <div className={cn("w-full p-2 bg-[repeating-linear-gradient(45deg,#c6fb04_0px,#c6fb04_20px,transparent_20px,transparent_35px)]", className)}></div>
         <div className="ml-4 min-w-3 bg-accent"></div>
       </div>
       <Extrusion className="mt-4 float-right min-w-[40%] border-accent" cornerLocation={CornerLocation.BottomLeft} />
