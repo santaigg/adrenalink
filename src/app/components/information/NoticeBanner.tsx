c7e0f9f3f33fe4c20db05a6a87e6921be7e116ed
Kadir Lofca <kadirlofca@outlook.com>
2025-01-17 22:23:45 -0500

    added lines to notice banner

--- src/app/components/information/NoticeBanner.tsx
+++ src/app/components/information/NoticeBanner.tsx
@@ -2,7 +2,7 @@ import { cn } from "../../utils/cn";
 
 export default function NoticeBanner({ className, noticeTitle, noticeBottomText }: { className?: string; noticeTitle: string; noticeBottomText?: string }) {
   return (
-    <div>
+    <div className="py-8 border-y border-accent border-opacity-50">
       <p className="mb-2 text-accent text-xl font-semibold">{noticeTitle}</p>
       <div className={cn("w-full h-12 opacity-60 bg-[repeating-linear-gradient(45deg,#c6fb04_0px,#c6fb04_20px,transparent_20px,transparent_35px)]", className)}></div>
       <p className="mt-2 text-accent">{noticeBottomText}</p>
