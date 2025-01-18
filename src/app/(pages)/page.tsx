4f7a9132c4a2f83434d324c72dcd855fc9f16858
Kadir Lofca <kadirlofca@outlook.com>
2025-01-17 22:38:28 -0500

    Added some islands to homepage

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -8,19 +8,23 @@ import NoticeBanner from "../components/information/NoticeBanner";
 
 export default function Home() {
   return (
-    <main>
+    <main className="h-full">
       <BackgroundImage image={BackgroundImageData} />
-      <Constrict className="my-12 px-4">
+      <Constrict className="my-8 px-4">
         <NoticeBanner className="text-center" noticeTitle="Santai.GG / Flashpoint" noticeBottomText="Brand new website, polished experience." />
       </Constrict>
-      <Constrict className="max-w-96">
-        <div className="p-secondary rounded-secondary text-primary-foreground">
-          <div className="flex flex-row">
-            <h2 className="mb-1 text-primary-foreground">Player Search</h2>
-            <Image src={SpectreLogoImage} alt="Spectre Divide wordmark." className="h-6 w-auto ml-auto mt-auto mb-2 brightness-75" />
+      <Constrict className="h-full px-4 flex flex-row gap-8">
+        <div className="flex-1 flex flex-col gap-8 text-primary-foreground">
+          <div>
+            <div className="flex flex-row">
+              <h2 className="mb-1 text-primary-foreground">Player Search</h2>
+              <Image src={SpectreLogoImage} alt="Spectre Divide wordmark." className="h-6 w-auto ml-auto mt-auto mb-2 brightness-75" />
+            </div>
+            <Searchbox placeholder="Username or Steam64..." />
           </div>
-          <Searchbox placeholder="Username or Steam64..." />
+          <div className="h-full p-4 bg-primary">Real time logs.</div>
         </div>
+        <div className="flex-1 h-full p-4 bg-primary">Global stats here.</div>
       </Constrict>
     </main>
   );
