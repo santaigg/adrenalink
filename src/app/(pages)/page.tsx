d2d2e0ba7fbef26f048f61b4de4feaa6ebce4fc9
Dom Ody <dom20.ody07@gmail.com>
2025-02-24 08:33:59 +0100

    initial ui changes

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -64,16 +64,16 @@ export default function Home() {
 
   return (
     <main className="h-full">
-      <BackgroundImage image={BackgroundImageData} />
-        <NoticeBanner
-          className="text-center"
-          noticeTitle="Santai.GG \\ Flashpoint"
-          noticeBottomText="Brand new website, polished experience."
-          ver={2}
-        />
-      <Constrict className="h-full px-4 flex flex-col sm:flex-row gap-8">
-        <div className="flex sm:flex-1 flex-col gap-8 text-primary-foreground">
-          <div>
+      <BackgroundImage className="max-h-[35rem]" image={BackgroundImageData} />
+      {/* <NoticeBanner
+        className="text-center"
+        noticeTitle="Santai.GG \\ Flashpoint"
+        noticeBottomText="Brand new website, polished experience."
+        ver={2}
+      /> */}
+      <div className="relative overflow-hidden w-full border-b border-secondary h-[30rem]">
+        <Constrict className="h-full px-4 flex items-start justify-start gap-8 space-x-16">
+          <div className="w-1/2 h-full flex justify-center flex-col">
             <div className="flex flex-row">
               <h2 className="mb-1 text-primary-foreground">Player Search</h2>
               <Image
@@ -83,93 +83,129 @@ export default function Home() {
               />
             </div>
             <Searchbox placeholder="Username or Steam64..." />
+            <div className="h-8 bg-accent rounded w-full mt-8 opacity-0 flex items-center justify-center text-accent-foreground text-sm font-medium"></div>
           </div>
-          {/* <div className="h-full p-4 bg-primary">Real time logs.</div> */}
-        </div>
-        <div className="flex flex-1 justify-start gap-y-4 flex-col">
-          <div className="h-6">
-            <h2 className="mb-1 text-primary-foreground">Top 3</h2>
-          </div>
-          {leaderboard.map((player, index) => {
-            const pfp = profileImages[index];
-            return (
-              <div
-                key={player.playerId}
-                className={`flex flex-col items-center w-full`}
-              >
-                {/* whole thing */}
-                <div
-                  className={`hover:-translate-y-2 transition-all w-full ${
-                    index === 0 ? "" : ""
-                  }`}
-                >
-                  {/* number on top */}
-                  <div
-                    className={`w-48 rounded-tl-primary text-input pl-4 py-0.5 flex justify-start items-center ${
-                      index === 0
-                        ? "bg-amber-500"
-                        : index === 1
-                        ? "bg-stone-500"
-                        : "bg-amber-900"
-                    }`}
-                    style={{
-                      clipPath:
-                        "polygon(0 0, calc(100% - 15px) 0%, 100% 15px, 100% 100%, 0% 100%)",
-                    }}
-                  >
-                    <p
-                      className={`${
-                        index === 0
-                          ? "text-amber-100"
-                          : index === 1
-                          ? "text-stone-100"
-                          : "text-amber-100"
-                      }`}
-                    >
-                      #{index + 1}
-                    </p>
-                  </div>
-                  {/* main body */}
-                  <div
-                    className="w-full rounded-tr-primary rounded-bl-primary bg-primary border-secondary border-y flex flex-col overflow-hidden hover:bg-primary/85 transition-all cursor-pointer"
-                    style={{
-                      clipPath:
-                        "polygon(100% 0%, 0% 0%, 0% 100%, calc(100% - 30px) 100%, 100% calc(100% - 30px))",
-                    }}
-                    onClick={() => router.push(`/p/${player.playerId}`)}
-                  >
-                    <div className="flex gap-x-4 items-center justify p-4 w-full">
-                      {pfp ? (
-                        <img
-                          src={profileImages[index]}
-                          className="size-20 corner-clip"
-                        ></img>
-                      ) : (
-                        <div className="size-20 bg-neutral-600 corner-clip animate-pulse"></div>
-                      )}
-
-                      <div className="flex flex-col items-start justify-center">
-                        <h2 className="">{player.username}</h2>
-                        <h3 className="">{player.rating} RR</h3>
-                      </div>
-                    </div>
-                  </div>
+          <div className="w-1/2 h-full flex justify-center items-center space-x-4">
+            {leaderboard.map((player, index) => {
+              const pfp = profileImages[index];
+              return (
+                <div className="w-36 h-56 bg-secondary/70 border border-primary-foreground/15 flex flex-col items-center rounded-primary px-4 py-4">
+                  {pfp ? (
+                    <img
+                      src={profileImages[index]}
+                      className="size-20 rounded-primary w-full object-cover corner-clip mx-auto"
+                    ></img>
+                  ) : (
+                    <div className="size-20 bg-neutral-600 corner-clip animate-pulse"></div>
+                  )}
+                  <p className="text-lg mt-2">{player.username}</p>
+                  <p className="text-2xl mt-2">{player.rating}</p>
                 </div>
-              </div>
-            );
-          })}
-          <button
-            className="bg-primary hover:bg-secondary/65 transition-all py-2 hover:text-accent flex justify-center items-center"
-            style={{
-              clipPath:
-                "polygon(15px 0%, 0% 15px, 0% 100%, calc(100% - 15px) 100%, 100% calc(100% - 15px), 100% 0%)",
-            }}
-            onClick={() => router.push(`/leaderboard`)}
-          >
-            Leaderboard
-          </button>
-        </div>
-      </Constrict>
+              );
+            })}
+          </div>
+        </Constrict>
+      </div>
     </main>
   );
 }
+
+// <Constrict className="h-full px-4 flex flex-col sm:flex-row gap-8">
+//   <div className="flex sm:flex-1 flex-col gap-8 text-primary-foreground">
+//     <div>
+//       <div className="flex flex-row">
+//         <h2 className="mb-1 text-primary-foreground">Player Search</h2>
+//         <Image
+//           src={SpectreLogoImage}
+//           alt="Spectre Divide wordmark."
+//           className="h-6 w-auto ml-auto mt-auto mb-2 brightness-75"
+//         />
+//       </div>
+//       <Searchbox placeholder="Username or Steam64..." />
+//     </div>
+//     {/* <div className="h-full p-4 bg-primary">Real time logs.</div> */}
+//   </div>
+//   <div className="flex flex-1 justify-start gap-y-4 flex-col">
+//     <div className="h-6">
+//       <h2 className="mb-1 text-primary-foreground">Top 3</h2>
+//     </div>
+//     {leaderboard.map((player, index) => {
+//       const pfp = profileImages[index];
+//       return (
+//         <div
+//           key={player.playerId}
+//           className={`flex flex-col items-center w-full`}
+//         >
+//           {/* whole thing */}
+//           <div
+//             className={`hover:-translate-y-2 transition-all w-full ${
+//               index === 0 ? "" : ""
+//             }`}
+//           >
+//             {/* number on top */}
+//             <div
+//               className={`w-48 rounded-tl-primary text-input pl-4 py-0.5 flex justify-start items-center ${
+//                 index === 0
+//                   ? "bg-amber-500"
+//                   : index === 1
+//                   ? "bg-stone-500"
+//                   : "bg-amber-900"
+//               }`}
+//               style={{
+//                 clipPath:
+//                   "polygon(0 0, calc(100% - 15px) 0%, 100% 15px, 100% 100%, 0% 100%)",
+//               }}
+//             >
+//               <p
+//                 className={`${
+//                   index === 0
+//                     ? "text-amber-100"
+//                     : index === 1
+//                     ? "text-stone-100"
+//                     : "text-amber-100"
+//                 }`}
+//               >
+//                 #{index + 1}
+//               </p>
+//             </div>
+//             {/* main body */}
+//             <div
+//               className="w-full rounded-tr-primary rounded-bl-primary bg-primary border-secondary border-y flex flex-col overflow-hidden hover:bg-primary/85 transition-all cursor-pointer"
+//               style={{
+//                 clipPath:
+//                   "polygon(100% 0%, 0% 0%, 0% 100%, calc(100% - 30px) 100%, 100% calc(100% - 30px))",
+//               }}
+//               onClick={() => router.push(`/p/${player.playerId}`)}
+//             >
+//               <div className="flex gap-x-4 items-center justify p-4 w-full">
+//                 {pfp ? (
+//                   <img
+//                     src={profileImages[index]}
+//                     className="size-20 corner-clip"
+//                   ></img>
+//                 ) : (
+//                   <div className="size-20 bg-neutral-600 corner-clip animate-pulse"></div>
+//                 )}
+
+//                 <div className="flex flex-col items-start justify-center">
+//                   <h2 className="">{player.username}</h2>
+//                   <h3 className="">{player.rating} RR</h3>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       );
+//     })}
+//     <button
+//       className="bg-primary hover:bg-secondary/65 transition-all py-2 hover:text-accent flex justify-center items-center"
+//       style={{
+//         clipPath:
+//           "polygon(15px 0%, 0% 15px, 0% 100%, calc(100% - 15px) 100%, 100% calc(100% - 15px), 100% 0%)",
+//       }}
+//       onClick={() => router.push(`/leaderboard`)}
+//     >
+//       Leaderboard
+//     </button>
+//   </div>
+// </Constrict>;
